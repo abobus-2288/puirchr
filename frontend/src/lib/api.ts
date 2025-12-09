c08a4d0fd7f5ece837711ec80cd07d3563bd1864
Eugene <ucta456@gmail.com>
2025-12-09 09:36:25 +0300

    fixes

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { AuthResponse, User, Category, Test, TestResult, PaginatedResponse } from '@/types';
 
-const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/v1` || 'http://localhost:8000/api';
+const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/v1` || 'http://testingsite.ru/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
